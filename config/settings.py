9f2ccd6fd3eccef250e3e126841815209834a142
HericMD <hericmdms@gmail.com>
2023-08-30 14:41:27 -0300

    media endpoint alterado para o ip

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
 ]
 
 # App Uploader settings
-MEDIA_URL = "http://localhost:8000/media/"
+MEDIA_URL = "http://191.52.55.35:19003/media/"
 MEDIA_ENDPOINT = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 FILE_UPLOAD_PERMISSIONS = 0o640
