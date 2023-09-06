cad4e963435355c5f39fffd1ad0ac62d6cdd8993
HericMD <hericmdms@gmail.com>
2023-09-06 13:11:38 -0300

    update model endereco

--- padaria/models/endereco.py
+++ padaria/models/endereco.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Endereco(models.Model):
-    numero = models.FloatField(max_length=6, null=False, blank=False)
+    numero = models.IntegerField(null=False, blank=False)
     complemento = models.CharField(max_length=100, null=True, blank=True)
-    cep = models.FloatField(max_length=8, null=False, blank=False)
+    cep = models.IntegerField(null=False, blank=False)
 
     def __str__(self):
         return f"{self.numero} {self.complemento} {self.cep}"
