f2594beeb12629bfaffac1eae95639b12a65d8ad
HericMD <hericmdms@gmail.com>
2023-10-12 14:35:19 -0300

    IntegerChoice para unidade de produto

--- padaria/models/produto.py
+++ padaria/models/produto.py
@@ -3,12 +3,13 @@ from uploader.models import Image
 from padaria.models import Categoria
 
 class Produto(models.Model):
+    class Unidade(models.IntegerChoices):
+        UNIDADE = (1, "Unidade",)
+        PESO = (2, "Peso (KG)")
     nome = models.CharField(max_length=255, null=False, blank=False)
-    descricao = models.CharField(max_length=50, null=False, blank=False)
     preco = models.DecimalField(max_digits=4, decimal_places=2, null=False, blank=False)
-
+    unidade = models.IntegerField(choices=Unidade.choices, default=Unidade.UNIDADE)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="produto", null=True, blank=True)
-    # imagem = models.ImageField(upload_to=(''), null=True, blank=True)
     imagem = models.ForeignKey(Image, on_delete=models.PROTECT, null=True, blank=True)
 
     def __str__(self):
