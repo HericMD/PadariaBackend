0112ab90488f311e8082660289ff8341d180cb0d
HericMD <hericmdms@gmail.com>
2023-10-23 16:07:47 -0300

    slugrelated field para .imagem e alterado  .cover

--- padaria/models/produto.py
+++ padaria/models/produto.py
@@ -10,7 +10,7 @@ class Produto(models.Model):
     preco = models.DecimalField(max_digits=4, decimal_places=2, null=False, blank=False)
     unidade = models.IntegerField(choices=Unidade.choices, default=Unidade.UNIDADE)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="produto", null=True, blank=True)
-    imagem = models.ForeignKey(Image, on_delete=models.PROTECT, null=True, blank=True)
+    cover = models.ForeignKey(Image, on_delete=models.PROTECT)
 
     def __str__(self):
         return f"{self.nome} {self.preco}"
--- padaria/serializers/produto.py
+++ padaria/serializers/produto.py
@@ -1,10 +1,20 @@
-from rest_framework.serializers import ModelSerializer, CharField
-
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, CharField, SlugRelatedField
+from uploader.models import Image
 from uploader.serializers import ImageSerializer
 
 from padaria.models import Produto
 
 class ProdutoSerializer(ModelSerializer):
+    cover_attachment_key = serializers.SlugRelatedField(
+        source="cover",
+        queryset=Image.objects.all(),       # pylint: disable=no-member
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    cover = ImageSerializer(required=False, read_only=True)
+
     class Meta:
         model = Produto
         fields = "__all__"
@@ -19,8 +29,17 @@ class ProdutoDetailSerializer(ModelSerializer):
 class ProdutoListSerializer(ModelSerializer):
     categoria = CharField(source="categoria.descricao", read_only=True)
     unidade = CharField(source="get_unidade_display", read_only=True)
+    cover_attachment_key = serializers.SlugRelatedField(
+        source="cover",
+        queryset=Image.objects.all(),       # pylint: disable=no-member
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    cover = ImageSerializer(required=False, read_only=True)
+
     class Meta:
         model = Produto
-        fields = ["id", "nome", "unidade", "preco", "imagem", "categoria"]
-        depth = 1
+        fields = "__all__"
+        depth=1
         
